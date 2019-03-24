d83e37b2a93bf76ca782230842f31163236a2a92
Đỗ Tấn Khoa <khoadt2@vng.com.vn>
2019-03-24 21:07:24 +0700

    create test add testcase

---
+++ DogYearsUnitTests/DogYearsUnitTests.swift
@@ -0,0 +1,35 @@
+//
+//  DogYearsUnitTests.swift
+//  DogYearsUnitTests
+//
+//  Created by Đỗ Tấn Khoa on 3/24/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+
+import XCTest
+@testable import DogYears
+
+class DogYearsUnitTests: XCTestCase {
+    
+    let calc = Calculator()
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAdd() {
+        let result = calc.evaluate(op: "+", arg1: 4.5, arg2: 9.0)
+        XCTAssert(result == 13.5, "Calculator add operation failed")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
